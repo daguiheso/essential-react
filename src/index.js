7b7e898c230144845eac36a4a410ebb7f4e43541
Daniel H <daguiheso@gmail.com>
2019-09-14 14:28:01 -0500

    send data to child component from parent

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import HelloWorld from './components/HelloWorld'
+import Button from './components/Button'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ReactDOM.render(<Button text="Click Me" />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
